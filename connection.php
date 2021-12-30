a9c2c6d1ef7c44a5a7bd14c5d9fff1f9e73cac17
Manish Kumar <manishegley@gmail.com>
2021-12-30 21:01:31 +0530

    Connection file3

--- connection.php
+++ connection.php
@@ -16,7 +16,7 @@
 
    $sql ="INSERT INTO resources (title,description,url,paging) VALUES ('Manish', 'jhghjghghb', 'httgh://mbj.com', 2);";
 
-   // $ret = pg_query($db, $sql);
+   $ret = pg_query($db, $sql);
    // if(!$ret) {
    //    echo pg_last_error($db);
    // } else {
