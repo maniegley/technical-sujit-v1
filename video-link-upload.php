534a589e3f6faabb6add8a49eebcc65d2bdffc39
Manish Kumar <manishegley@gmail.com>
2022-01-06 18:35:32 +0530

    video adding file

---
+++ video-link-upload.php
@@ -0,0 +1,109 @@
+<?php
+    $title = "";
+    $description = "";
+    $target_dir = "";
+    $url = "";
+    $errors = array(); 
+    $success = array(); 
+
+// connect to the database
+//$db = mysqli_connect('localhost', 'root', '', 'barber');
+
+// video uploading
+    if (isset($_POST['add-video-link'])) {
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+        $url = $_POST['url'];
+        $uploadOk = 1;
+        if (empty($title)) {
+            array_push($errors, "Title is required");
+        }
+
+        if (empty($description)) {
+            array_push($errors, "Description is required");
+        }
+
+        if (empty($url)) {
+            array_push($errors, "URL is required");
+        }
+
+        if (empty(basename($_FILES["fileToUpload"]["name"]))) {
+            array_push($errors, "Thumbnail image is required. ");
+            $uploadOk = 0;
+        }
+
+        $target_dir = "images/";
+        //echo gettype(basename( $_FILES["fileToUpload"]["name"]));
+        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+        
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+        // Check if image file is a actual image or fake image
+        if(isset($_POST["submit"])) {
+            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+            if($check !== false) {
+                //echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } else {
+                array_push($errors, "File is not an image.");
+                $uploadOk = 0;
+            }
+        }
+
+        if($uploadOk==1){
+            // Check file size
+            if ($_FILES["fileToUpload"]["size"] > 500000) {
+                array_push($errors,"Sorry, your file is too large.");
+                $uploadOk = 0;
+            }
+        }
+
+        if($uploadOk==1){
+            // Allow certain file formats
+            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif" ) {
+                array_push($errors,"Sorry, only JPG, JPEG, PNG & GIF files are allowed.");
+                $uploadOk = 0;
+            }
+        }
+
+        if($uploadOk==1){
+            // Check if file already exists
+            if (file_exists($target_file)) {
+                array_push($errors,"Sorry, file already exists.");
+                $uploadOk = 0;
+            }
+        }
+
+        if($uploadOk==1){
+            // Check if $uploadOk is set to 0 by an error
+            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+                $img = $target_file;
+            } else {
+                    array_push($errors,"Sorry, there was an error uploading your file.");
+                }
+            }
+
+
+$sub_admin = "admin";
+if (count($errors) == 0) {
+  $resource_query = "INSERT INTO t_vid_link (vid_title,vid_description,vid_url,vid_img,vid_uploaded_by) VALUES ('$title', '$description', '$url', '$img','$sub_admin');";
+  echo $resource_query;
+  include('connection.php');
+    if(!$db) {
+      array_push($errors, "Error : Unable to make connection!\n");
+    } else {
+        echo "Opened database successfully\n";
+    }
+    $result = pg_query($db, $resource_query);
+        //echo $resource_query;
+        if(!$result) {
+          array_push($errors, "Something went wrong! Please try again.");
+            } else {
+                array_push($success, "Video uploaded successfully.");
+                header('location: successfull-trasaction.php');
+            }
+}
+}
+?>
