5b8bfc8ce39c72d731125baa5f6d466056a40466
Manish Kumar <manishegley@gmail.com>
2022-01-07 12:22:29 +0530

    Adding database connection file

--- connection.php
+++ connection.php
@@ -2,26 +2,28 @@
 
 
 <?php
+// For Postgres SQL connection
    $host        = "host = ec2-3-237-55-151.compute-1.amazonaws.com";
    $port        = "port = 5432";
    $dbname      = "dbname = d1a1jjajqe2in";
    $credentials = "user = kvqhfucaajbtaa password=38b32834de960a958c914b1c62b405de6357db95c686c05210ee4cedf02a53be";
-
    $db = pg_connect("$host $port $dbname $credentials" );
-   if(!$db) {
-      echo "Error : Unable to open database\n";
-   } else {
-      echo "Opened database successfully\n";
-   }
+   // $host        = "host = localhost";
+   // $port        = "port = 5432";
+   // $dbname      = "dbname = api";
+   // $credentials = "user = postgres password= 1234";
+   // $db = pg_connect("$host $port $dbname $credentials" );
 
-   $sql ="INSERT INTO resources (title,description,url,paging) VALUES ('Manish', 'jhghjghghb', 'httgh://mbj.com', 2);";
+   // For MySQL connection
 
-   $ret = pg_query($db, $sql);
-   if(!$ret) {
-      echo pg_last_error($db);
-   } else {
-      echo "Records created successfully\n";
-      echo "httgh://mbj.com";
-   }
-   pg_close($db);
+   // $server_name="localhost";
+
+	// $user_name="root";
+
+	// $password="";
+
+	// $db_name="technicalsujit";
+
+	// $db=new mysqli($server_name,$user_name,$password,$db_name);
+
 ?>
