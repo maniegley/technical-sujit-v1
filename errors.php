de3ab20f4454b1b971bfba41812620b3a0f21c57
Manish Kumar <manishegley@gmail.com>
2022-01-11 13:27:20 +0530

    change style and error page

--- errors.php
+++ errors.php
@@ -5,3 +5,10 @@
   	<?php endforeach ?>
   </div>
 <?php  endif ?>
+<?php  if (count($success) > 0) : ?>
+  <div class="success">
+  	<?php foreach ($success as $sucess) : ?>
+  	  <p><?php echo $sucess ?></p>
+  	<?php endforeach ?>
+  </div>
+<?php  endif ?>
