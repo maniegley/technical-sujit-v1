ae5c77b6bdbc938bee2112158638d1a833324628
Manish Kumar <manishegley@gmail.com>
2022-01-11 13:16:13 +0530

    make connection changes

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php 
+    $page = 0;
+    
+    include('connection.php');
+    
+    if(!$db) {
+        echo "Error : Something went wrong. Unable to make connection!\n";
+    } else {
+        //echo "Opened database successfully\n";
+        $get_video_query = "SELECT * FROM t_vid_link ORDER BY id DESC LIMIT 4";
+        //$result = mysqli_query($db, $get_video_query);
+        $result = pg_query($db, $get_video_query);
+        //$c = 1;
+        // while($row = mysqli_fetch_array($result)){
+?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -7,161 +22,64 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <script src="bootstrap-3.3.7\dist\js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="style/mystyle.css">
         <link rel="icon" href="images/logo1.jpeg" sizes="32x32" />
     </head>
     <body>
 
-        <?php include('header.php');?>
-  
-        
-      
-                    <div class="row row-item">
-                        <div class="col-sm-7">
-                            <div class="well1">
-                                <a href="https://youtu.be/JW8wTpFo22I">
-                                    <img src="images/thumbnail1.jpeg" class="" height="200" width="300" alt="Avatar">
-                                </a>
-                                <p><strong>Uploded :</strong> 20 December 2021</p>
-                                <a href="https://youtu.be/JW8wTpFo22I"><button class="btn btn-primary">Watch Now</button></a>
-                            </div>
-                        </div>
-                        <div class="col-sm-5">
-                            <div class="well1">
-                                <p><a href="https://youtu.be/JW8wTpFo22I"><span class="glyphicon glyphicon-hand-right"> </span><u><strong> slow motion fast motion video kaise banaye | slow fast video editing tutorial | slow mo video editor</strong></u></a></p>
-                                <p style="color:white;">Is video mein maine bataya hai ki....slow motion fast motion video kaise banaye | slow fast video editing tutorial | slow mo video editing 
-                                </p>
-                                <p><strong>Uploded :</strong> 20 December 2021</p>
-                                <a href="https://youtu.be/JW8wTpFo22I"><button class="btn btn-primary">Watch Now</button></a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row row-item">
-                        <div class="col-sm-7">
-                            <div class="well1">
-                                <a href="https://youtu.be/5y0QslxMvOc">
-                                    <img src="images/thumbnail2.jpeg" class="" height="200" width="300" alt="Avatar">
-                                </a>
-                                <p><strong>Uploded :</strong> 16 December 2021</p>
-                                <a href="https://youtu.be/5y0QslxMvOc"><button class="btn btn-primary">Watch Now</button></a>
-                            </div>
-                        </div>
-                        <div class="col-sm-5">
-                            <div class="well1">
-                                <p><a href="https://youtu.be/5y0QslxMvOc"><span class="glyphicon glyphicon-hand-right"> </span><u><strong> introducing my love of my life reel | introducing my love of my life reels video kaise banaye</strong></u></a></p>
-                                <p style="color:white;">Is video mein maine bataya hai ki..introducing my love of my life reel | introducing my love of my life reels video kaise banaye 
-                                </p>
-                                <p><strong>Uploded :</strong> 16 December 2021</p>
-                                <a href="https://youtu.be/5y0QslxMvOc"><button class="btn btn-primary">Watch Now</button></a>
-                            </div>
-                     
-                        </div>
-                    </div>
-                    <div class="row row-item">
-                        <div class="col-sm-7">
-                            <div class="well1">
-                                <a href="https://youtu.be/QGm5h_MO3pI">
-                                    <img src="images/thumbnail3.jpeg" class="" height="200" width="300" alt="Avatar">
-                                </a>
-                                <p><strong>Uploded :</strong> 14 December 2021</p>
-                                <a href="https://youtu.be/QGm5h_MO3pI"><button class="btn btn-primary">Watch Now</button></a>
-                            </div>
-                        </div>
-                        <div class="col-sm-5">
-                            <div class="well1">
-                                <p><a href="https://youtu.be/QGm5h_MO3pI"><span class="glyphicon glyphicon-hand-right"> </span><u><strong> instagram reels name art video editing | chand wala mukhda name art video editing | reels new trend</strong></u></a></p>
-                                <p style="color:white;">Is video mein maine bataya hai ki....instagram reels name art video editing | chand wala mukhda name art video editing | reels new trend  
-                                </p>
-                                <p><strong>Uploded :</strong> 14 December 2021</p>
-                                <a href="https://youtu.be/QGm5h_MO3pI"><button class="btn btn-primary">Watch Now</button></a>
+        <?php include('header.php'); ?>
+        <div class="container text-center row-item" style="margin-top:5px;">    
+            <div class="row">
+                <div class="col-sm-offset-1 col-sm-7">
+                    <?php
+                        while($row = pg_fetch_assoc($result)){
+                    ?>
+                    <div class="row row-item1">
+                        <div class="panel">
+                            <div class="panel-body">
+                                <div class="col-sm-5">
+                                    <a href="<?php echo $row['vid_url'] ?>">
+                                        <img src="<?php echo $row['vid_img']; ?>" class="" alt="Avatar">
+                                    </a>
+                                    <p style="color: rgb(100, 86, 86);"><strong>Uploded On </strong><?php echo date_format(date_create($row['vid_post_date']), "d F Y"); ?>
+                                        <a href="<?php echo $row['vid_url'] ?>"><strong>  Watch Now</strong></a>
+                                    </p>
+                                </div>
+                                <div class="col-sm-5">    
+                                    <p>
+                                        <a href="<?php echo $row['vid_url'] ?>">
+                                            <span class="glyphicon glyphicon-hand-right"> </span>
+                                            <u>
+                                                <strong> <?php echo $row['vid_title']; ?></strong>
+                                            </u>
+                                        </a>
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
-
-                    <div class="row row-item" id="some">
-                        <div class="col-sm-7">
-                            <div class="well1">
-                                <a href="https://youtu.be/lGjBLviuieo">
-                                    <img src="images/thumbnail4.jpeg" class="" height="200" width="300" alt="Avatar">
-                                </a>
-                                <p><strong>Uploded :</strong> 11 December 2021</p>
-                                <a href="https://youtu.be/lGjBLviuieo"><button class="btn btn-primary">Watch Now</button></a>
-                            </div>
-                        </div>
-                        <div class="col-sm-5">
-                            <div class="well1">
-                                <p><a href="https://youtu.be/lGjBLviuieo"><span class="glyphicon glyphicon-hand-right"> </span><u><strong> how to create a photo scrolling whatsapp status | scrolling whatsapp status editing | Photo status editing</strong></u></a></p>    
-                                <p style="color:white;">Is video mein maine bataya hai ki....
-                                    how to create a photo scrolling whatsapp status | 
-                                    scrolling whatsapp status editing | Photo status editing
-                                </p>
-                                <p><strong>Uploded :</strong> 11 December 2021</p>
-                                <a href="https://youtu.be/lGjBLviuieo"><button class="btn btn-primary">Watch Now</button></a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row row-item" id="some">
-                        <div class="col-sm-7">
-                            <div class="well1">
-                                <a href="https://youtu.be/wxq8w3x7BjQ">
-                                    <img src="images/thumbnail5.jpeg" class="" height="200" width="300" alt="Avatar">
-                                </a>
-                                <p><strong>Uploded :</strong> 11 December 2021</p>
-                                <a href="https://youtu.be/wxq8w3x7BjQ"><button class="btn btn-primary">Watch Now</button></a>
-                            </div>
-                        </div>
-                        <div class="col-sm-5">
-                            <div class="well1">
-                                <p><a href="https://youtu.be/wxq8w3x7BjQ"><span class="glyphicon glyphicon-hand-right"> </span><u><strong> Reels viral sky running video editing | Cloud moving kaise banaye | Cloud moving video tutorial</strong></u></a></p>    
-                                    <p style="color:white;">Is video mein maine bataya hai ki....
-                                    Reels viral sky running video editing | Cloud moving kaise banaye | 
-                                    Cloud moving video editing tutorial
-                                </p>
-                                <p><strong>Uploded :</strong> 11 December 2021</p>
-                                <a href="https://youtu.be/wxq8w3x7BjQ"><button class="btn btn-primary">Watch Now</button></a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row row-item" id="some">
-                        <div class="col-sm-7">
-                            <div class="well1">
-                                <a href="https://youtu.be/SD6y3d_aO5U">
-                                    <img src="images/thumbnail6.jpeg" class="" height="200" width="300" alt="Avatar">
-                                </a>
-                                <p><strong>Uploded :</strong> 11 December 2021</p>
-                                <a href="https://youtu.be/SD6y3d_aO5U"><button class="btn btn-primary">Watch Now</button></a>
-                            </div>
-                        </div>
-                        <div class="col-sm-5">
-                            <div class="well1">
-                                <p><a href="https://youtu.be/SD6y3d_aO5U"><span class="glyphicon glyphicon-hand-right"> </span><u><strong> How To Make Tweets Video For Instagram Reels | Fake Tweet Video Editing | Reels New Trend</strong></u></a></p>    
-                                <p style="color:white;">Is video mein maine bataya hai ki....
-                                               How To Make Tweets Video For Instagram Reels | Fake Tweet Video Editing | Reels New Trend              
-                                </p>
-                                <p><strong>Uploded :</strong> 11 December 2021</p>
-                                <a href="https://youtu.be/SD6y3d_aO5U"><button class="btn btn-primary">Watch Now</button></a>
-                            </div>
-                        </div>
-                    </div>
+                    <?php }
+                    }
+                    ?>
                     <div class="row">
-                    <div class="col-sm-7">
-                        <div class="">                   
-                            <ul class="pager">
-                                <li><a href="#">Previous</a></li>
-                                <li><a href="page-1.php">Next</a></li>
-                            </ul>
+                        <div class="col-sm-7">
+                            <div class="">                   
+                                <ul class="pager">
+                                    <?php if($page==0){?>
+                                        <li><a href="index.php" style="pointer-events: none; cursor: default;">Previous</a></li>
+                                        <?php  } else{ ?>
+                                        <li><a href="index.php?page-no=<?php echo $page-1;?>">Previous</a></li><?php } ?>
+                                        <li><a href="index.php?page-no=<?php echo $page+1; ?>">Next</a></li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                </div>
                 </div>
                 
 
-
-                <div class="col-sm-3 well">
+                <!-- Right Pannel Start here! -->
+                <div class="col-sm-offset-1 col-sm-3 well">
                     <div class="thumbnail">
                         <p><strong>Most Viewed Video:</strong></p>
                         <iframe width="250" height="200"
@@ -175,32 +93,23 @@
 
                     <div class="thumbnail">
                         <p><strong style="color:green;">Recent Videos:</strong></p>
+
+                        <?php
+                            $get_video_query = "SELECT * FROM t_vid_link ORDER BY id DESC LIMIT 4";
+                            //$result = mysqli_query($db, $get_video_query);
+                            $result = pg_query($db, $get_video_query);
+                            while($row = pg_fetch_assoc($result)){
+                        ?>
+                        <!-- <img src="<?php echo $row['vid_img']; ?>" alt="Paris" width="400" height="300"> -->
                         <img src="images/thumbnail1.jpeg" alt="Paris" width="400" height="300">
-                        <p>26 November 2021</p>
-                        <button class="btn btn-primary">Info</button>
-                        
-                        <!-- <iframe width="250" height="200"
-                            src="https://www.youtube.com/embed/NjcRLyrqWv8">
-                        </iframe> -->
-                        <img src="images/thumbnail1.jpeg" alt="Paris" width="400" height="300">
-                        
-                        <p>02 December 2021</p>
-                        <button class="btn btn-primary">Info</button>
+                        <p style="color: rgb(100, 86, 86);"><strong>Uploded On  </strong><?php echo date_format(date_create($row['vid_post_date']), "d F Y"); ?></p>
 
-                        <img src="images/thumbnail2.jpeg" alt="Paris" width="400" height="300">
-                        <p>10 December 2021</p>
-                        <button class="btn btn-primary">Info</button>
-
-                        <img src="images/thumbnail3.jpeg" alt="Paris" width="400" height="300">
-                        <p>26 November 2021</p>
-                        <button class="btn btn-primary">Info</button>
-                    </div>         
-
-                             
-
-                            
+                        <?php }
                     
+                    ?>
+                    </div>             
                 </div>
+                <!-- Right Pannel ends here! -->
             </div>
         </div>
 
