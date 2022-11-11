4d6c37431448782187b7eb6a16a5f2e34338b467
Manish Kumar <manishegley@gmail.com>
2022-11-11 23:07:07 +0530

    blog footer is separated

--- blogs/2022/06-11-2022.php
+++ blogs/2022/06-11-2022.php
@@ -162,7 +162,7 @@
                 <!-- Right Pannel ends here! -->
             </div>
         </div> 
-        <?php include('../../footer.php');?>
+        <?php include('../footer.php');?>
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
