4a29f323f2116a2d8bb9efdbc0e5779411b0f73c
Manish Kumar <manishegley@gmail.com>
2022-12-04 17:19:27 +0530

    Added contact  page

--- contact-us.php
+++ contact-us.php
@@ -119,13 +119,89 @@ input[type=submit]:hover {
         </style>
     </head>
     <body>
-        <?php include('header.php');?>
-        <div class="container text-center row-item1" style="margin-top:5px;">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>                        
+                    </button>
+                    <a class="navbar-brand ml2" href="index.php">
+                        Technical Sujit
+                        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+                        <script src="js/myscripts.js"></script> -->
+                    </a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="index.php">Home</a></li>
+                        <!-- <li class=""><a href="resources.php">Resources</a></li> -->
+                        <li class="active"><a href="contact-us.php">Contact Us</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="privacy-policy.php">Privacy & Policy</a></li>
+                    </ul>
+                    <!-- <form class="navbar-form navbar-right" role="search">
+                        <div class="form-group input-group">
+                            <input type="text" class="form-control" placeholder="Search..">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="button">
+                                    <span class="glyphicon glyphicon-search"></span>
+                                </button>
+                            </span>        
+                        </div>
+                    </form> -->
+                    <!-- <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> My Account</a></li>
+                    </ul> -->
+                </div>
+            </div>
+        </nav>
+
+        <div class="container text-center" style="margin-top:60px;">    
+            <div class="row">
+                <div class="col-sm-offset-1 col-sm-8">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="panel text-left" style="width:300px; height:50px;">
+                                <div class="panel-body">
+                                    <img src="upload/logo1.jpeg" style="width:300px; height:90px;">
+                                </div>   
+                            </div>
+                    
+                        </div>
+                    </div>
+                    <!-- <div class="row">
+                        <div class="col-sm-12">
+                            <div class="panel panel-warning text-left">
+                                <div class="panel-body">
+                                    <p><strong style="color:red;">Recent Videos: </strong>
+                                        <a class="ml3" href="#">Tere Bhaiya Khade Hai Reels Editing</a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>  -->
+                    <div class="row row-item1">
+                        <h1 class="ml3 logo2" style="font-size:25px;">Learn Video editing with us!!!</h1>    
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container row-item1" style="margin-top:5px;">
             <div class="row">
                 <div class="col-sm-8 scrol">
-                    <h3>Contact Form</h3>
+                    <h1>Contact Us</h1>
+                    <p>We would like to make sure you are fully aware of all of your data protection rights. And we 
+                        beleive in fair rights. We know that we are not perfect but we try hard to improve ourseleves as per 
+                        your feedback. Therefore should not hesitate to contact us and can give your valuable feedback through
+                        email or phone no.
+                    </p>
 
-                    <div class="container">
+                    <h3>Email to : technicalsujit@gmail.com <a href="https://mail.google.com/mail/u/2/?zx=9gndovmtaxs#inbox?compose=new" target="_blank"><i class="fa fa-send-o"></i></a></h3>
+                    <h3>Phone : +91 6200821820</h3>
+                    </br></br></br></br></br></br>
+                    <!-- <div class="container">
                         <form action="/action_page.php">
                             <label for="fname">First Name</label>
                             <input type="text" id="fname" name="firstname" placeholder="Your name..">
@@ -144,7 +220,7 @@ input[type=submit]:hover {
                             <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
                             <input type="submit" value="Submit">
                         </form>
-                    </div>
+                    </div> -->
                 </div>  
 
                 <!-- Right Pannel Start here! -->
@@ -153,11 +229,11 @@ input[type=submit]:hover {
                         <h2 class="title">Follow Us</h2>
                     </div>
                     <div class="social_box">
-                        <a href="#" class="fa fa-facebook"></a>
-                        <a href="#" class="fa fa-twitter"></a>
-                        <a href="#" class="fa fa-google"></a>
-                        <a href="#" class="fa fa-linkedin"></a>
-                        <a href="#" class="fa fa-youtube"></a>
+                        <a href="https://www.facebook.com/technicalsujiteditor" class="fa fa-facebook" target="_blank"></a>
+                        <a href="https://www.instagram.com/invites/contact/?i=zxlr375unl9t&utm_content=nrq0xa2" class="fa fa-instagram" target="_blank"></a>
+                        <a href="https://t.me/TechnicalSujitBhai" class="fa fa-telegram" target="_blank"></a>
+                        <!-- <a href="#" class="fa fa-linkedin" target="_blank"></a> -->
+                        <a href="https://www.youtube.com/@TechnicalSujitReels/" class="fa fa-youtube" target="_blank"></a>
                     </div>
                 </div>
                 <!-- Right Pannel ends here! -->
